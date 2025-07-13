f23f3486bb5e6e6522ec309e2ab6be889c72bed0
SusithD <iamsusithalwis@gmail.com>
2025-07-13 19:28:26 +0530

    Refactor MongoDB connection handler to return connection status and model names

--- server/api/auth/test-mongo.get.js
+++ server/api/auth/test-mongo.get.js
@@ -1,8 +1,11 @@
-import { getDb } from '../../utils/mongodb'
+import { connectMongoose } from '../../utils/mongodb'
+import mongoose from 'mongoose'
 
 export default defineEventHandler(async (event) => {
-  const db = await getDb()
-  // Try to list collections or count documents in a collection
-  const collections = await db.listCollections().toArray()
-  return { status: 'success', collections }
+  await connectMongoose()
+  // Return connection status and list of model names
+  return {
+    status: mongoose.connection.readyState === 1 ? 'connected' : 'not connected',
+    models: Object.keys(mongoose.models)
+  }
 })
